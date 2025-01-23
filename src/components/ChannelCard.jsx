d6a151d2953358c40a236b5109d737d9bc48a324
Louis Romeo <146037158+louisromeo1@users.noreply.github.com>
2025-01-23 09:14:15 -0500

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -10,7 +10,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
           <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || '' } alt={channelDetail?.snippet?.title} 
           sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3'}}/>
             <Typography variant="h6">
-              {channelDetail?.snippet?.title || "Channel Name" }
+              {channelDetail?.snippet?.title}
               <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
             </Typography>
             {channelDetail?.statistics?.subscriberCount && (
